b084b55338dad2ca822f70c08220cbbed24c680b
Natsumi Hirano <hirano@forif.co.jp>
2012-10-02 13:50:55 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macaron::Application.config.secret_token = 'd50db5e61683daa5c6a4bd2d02af40fdda12eac9f02c8d543b1dbf1ef798db27461b6710998093df45022cff1490624fa27a5ff97627701ae9b4c819713d1147'
